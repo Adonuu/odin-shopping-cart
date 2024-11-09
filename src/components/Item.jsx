2b70fe6c3aa04d464b0f9392daab25410e650365
Adam Young <adamyoung1414@gmail.com>
2024-11-09 13:11:26 -0500

    removed boiler plate from vite template added api.js to query FakeStore API added Item.jsx for displaying an item from the api with states for quantity and a button to add to cart

---
+++ src/components/Item.jsx
@@ -0,0 +1,29 @@
+import PropTypes from "prop-types";
+import { useState } from "react";
+import styles from "./Item.module.css";
+
+export function Item( { product }) {
+    const [amount, setAmount] = useState(1);
+
+    function quantityChanged(e) {
+        e.preventDefault();
+        setAmount(e.target.value);
+    }
+
+    return(
+        <div className={styles.item}>
+            <img src={product.image} alt={product.title} />
+            <div>{product.title}</div>
+            <div>Price: ${product.price}</div>
+            <div className={styles["item-quantity"]}>
+                <label>Quantity:</label>
+                <input type="number" value={amount} onChange={quantityChanged} />
+            </div>
+            <button>Add To Cart</button>
+        </div>
+    )
+}
+
+Item.propTypes = {
+    product: PropTypes.object
+}
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { fetchItem } from './api.js'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
 )
+
+fetchItem(2);
