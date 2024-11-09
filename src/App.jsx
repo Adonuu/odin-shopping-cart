5c4177d206fa5c935bf2ddc043814d1945f38ca1
Adam Young <adamyoung1414@gmail.com>
2024-11-09 16:05:36 -0500

    shop items now displaying on shop page

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,34 @@ import { Header } from './components/Header/Header';
 import { Home } from './pages/Home/Home';
 import { Shop } from './pages/Shop/Shop';
 import './App.css';
+import { useEffect, useState } from 'react';
+import { fetchItem } from './api';
+
+const itemIds = [1, 2, 3, 4, 16, 18, 19, 20];
 
 function App() {
+
+  const [items, setItems] = useState([]);
+
+  async function getItems() {
+    const itemsList = [];
+    for (const id of itemIds) {
+      const data = await fetchItem(id);
+      itemsList.push(data);
+    }
+    setItems(itemsList);
+  }
+
+  useEffect(() => {
+    getItems()
+  }, [])
+
   return (
     <Router>
       <Header></Header>
       <Routes>
           <Route path="/" element={<Home></Home>}></Route>
-          <Route path="/shop" element={<Shop></Shop>}></Route>
+          <Route path="/shop" element={<Shop items={items}></Shop>}></Route>
       </Routes>
     </Router>
   );
--- src/pages/Shop/Shop.jsx
+++ src/pages/Shop/Shop.jsx
@@ -1,7 +1,20 @@
-export function Shop() {
+import PropTypes from "prop-types"
+import { Item } from "../../components/Item"
+import styles from "./Shop.module.css"
+
+export function Shop({ items }) {
     return (
-        <div className="main-content">
-            <h1>Shop</h1>
+        <div className={styles["main-content"]}>
+            {items.map((item, index) => (
+                <Item
+                key={index}
+                product={item}>
+                </Item>
+            ))}
         </div>
     )
+}
+
+Shop.propTypes = {
+    items: PropTypes.object
 }
