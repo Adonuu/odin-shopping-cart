f6c991fe31f6a0acf1af2a0e869536180c3e4c5f
Adam Young <adamyoung1414@gmail.com>
2024-11-09 16:55:34 -0500

    fix for displyaing photo properly on vercel

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,10 @@
 import styles from "./Home.module.css"
+import image from "/images/home.jpg"
 
 export function Home() {
     return (
         <div className={styles["main-content"]}>
-            <img src="/images/home.jpg" alt="Frog in Rainforest"></img>
+            <img src={image} alt="Frog in Rainforest"></img>
             <p>At Hypers we are focused on the future of this planet&apos;s rainforests. That is why our products use materials that are 100% sustainable. We also donate a tree for every dollar spent on our site.</p>
         </div>
     )
